fbe7c9c9fb32a28dc47616f98baeaba9bbed3654
ttronexe2 <adria.baltrons@gracia.lasalle.cat>
2024-10-22 23:27:01 +0200

    feat: :sparkles: MyFirstAPI

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const students = [
+    "Adrià Baltrons",
+    "Nil Marquez",
+    "Joshua Abril",
+    "Mateo Achá",
+    "Arnau Gil",
+    "Joan Cookies",
+    "Adrián Lozano",
+    "Paul Francés",
+    "Satenik Satenik",
+    "Pol Cubo",
+    "Roger Bustos",
+    "Nico Atletico"
+];
+
+app.get('/class', (req, res) => {
+    res.json({
+        students: students,
+        totalStudents: students.length
+    });
+});
+
+app.listen(port, () => {
+    console.log(`API running on http://localhost:${port}`);
+});
